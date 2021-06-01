a406df85c859913a4688508db670d3107707004a
subhasish <subhasish.kundu3@gmail.com>
2021-06-02 01:11:59 +0530

    Todo Item CX v1.

--- todoy-front-end/src/api/constants.js
+++ todoy-front-end/src/api/constants.js
@@ -4,7 +4,7 @@ export const SERVER_URL = IS_PROD
   ? // In production we want to redirect to /whatever
     ``
   : // In development we gotta redirect to localhost:3001/whatever tho
-    'http://localhost:9000';
+    'http://localhost:9000 ';
 
 export const CLIENT_URL = IS_PROD
   ? `${window.location.protocol}//${window.location.host}`
--- todoy-front-end/src/components/Item.js
+++ todoy-front-end/src/components/Item.js
@@ -2,15 +2,42 @@ const Item = (props) => {
     return (
         <div className="card white darken-1">
             <div className="card-content black-text">
-                <span className="card-title left-align">{props.title}<i className="material-icons right">more_vert</i></span>
+                <span className="card-title activator left-align">{props.title}<i className="material-icons right">more_vert</i></span>
                     <div>
                         <span className="left"><p>{props.category}</p></span>
                         <span className="right"><p>{props.state}</p></span>
                         <br/>
                     </div>
             </div>
+            <div class="card-reveal">
+                <span className="card-title grey-text text-darken-4">
+                    <i class="material-icons right">close</i>
+                </span>
+                <p>Edit or delete your todo item.</p>
+                <div className="row">
+                    <div className="col s6"><ButtonComponentOfTodoItem type="edit" /></div>
+                    <div className="col s6"><ButtonComponentOfTodoItem type="delete" color="red"/></div>
+                </div>
+            </div>
         </div>
     )
+}
+
+const ButtonComponentOfTodoItem = (button) => {
+    let className = "btn-floating waves-effect waves-light btn-small ";
+    if(button.color) {
+        className += button.color
+    } else {
+        className += "green"
+    }
+    return (
+        <>
+            <a className={className}>
+                <i className="material-icons right">
+                    {button.type}
+                </i>
+            </a>
+        </>);
 }
 
 export default Item;
